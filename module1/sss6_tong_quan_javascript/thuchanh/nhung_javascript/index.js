d37ef3d7e764f1180865ef1a380449491ff37e4d
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-05-13 11:08:13 +0700

    HPT commit ss5,ss6 13/05/2022

---
+++ module1/sss6_tong_quan_javascript/thuchanh/nhung_javascript/index.js
@@ -0,0 +1,18 @@
+ // Bài 1
+ function showMessage_Bai1(){
+    alert('Xin chào! - Bài 1');
+}
+// Bài 2
+var a = document.getElementById('messageDiv');
+
+function showMessage_Bai2(){
+    a.innerHTML = 'Xin chào ! - Bài 2'
+}
+// Bài 3
+function showMessage_Bai3(){
+    document.write('Xin chào! - Bài 3');
+}
+// Bài 4
+function showMessage_Bai4(){
+    console.log('Xin chào! - Bài 4');
+}
