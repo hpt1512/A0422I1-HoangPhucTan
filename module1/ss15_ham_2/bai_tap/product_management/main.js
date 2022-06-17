3d9f5ebf069cc34999eda112e011d64223447139
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-06-17 20:12:31 +0700

    Commit ss15 Product Management

--- module1/ss15_ham_2/bai_tap/product_management/main.js
+++ module1/ss15_ham_2/bai_tap/product_management/main.js
@@ -1,41 +1,39 @@
-let productArray = [];
-// let table_product = document.getElementById('table_product');
+let productArray = ['samsung','nokia','lenovo'];
 let all_product = document.getElementById('all_product');
 
-let data = "";
-
-
-let btn_add = document.getElementById('btn_add');
-btn_add.addEventListener('click', addNewProduct);
-function addNewProduct() {
+// Show
+function showProduct() {
     let data = "<table >";
-    let newProduct = document.getElementById('addProduct').value;
+    let countProduct = document.getElementById('countProduct');
+    for(let i = 0; i < productArray.length; i++) {
 
-    productArray.push(newProduct);
-
-    for (let i = 0; i < productArray.length; i++) {
         data = data + `<td style="width: 100px;">` + productArray[i] + `</td>`
-            + `<td style="width: 100px;">` + '<button class="btn-edit">Edit</button>' + "</td>"
-            + `<td style="width: 100px;">` + '<button class="btn-delete">Delete</button>' + "</td>";
-
+                 + `<td style="width: 100px;">` + '<button class="btn-edit" id="btn_edit">Edit</button>' + "</td>"
+                 + `<td style="width: 100px;">` + '<button class="btn-delete" id="btn_delete">Delete</button>' + "</td>" ;
+        
         data = data + "</tr>";
+
     }
 
-    // data = data + `<td style="width: 100px;">` + newProduct + `</td>`
-    //     + `<td style="width: 100px;">` + '<button class="btn-edit">Edit</button>' + "</td>"
-    //     + `<td style="width: 100px;">` + '<button class="btn-delete">Delete</button>' + "</td>";
+    console.log(productArray);
 
-    // data = data + "</tr>";
-    data = data + "</table> <br>";
+    data = data + "</table>";
 
-
-
-
-    document.getElementById('addProduct').value = '';
-
-    // document.write(data);
     all_product.innerHTML += data;
+    countProduct.innerHTML = productArray.length;
+}
+
+showProduct();
+
+// Add
+let btn_add = document.getElementById('btn_add');
+btn_add.addEventListener('click', addNewProduct);
+
+function addNewProduct() {
+    let newProduct = document.getElementById('addProduct').value;
+    productArray.push(newProduct);
+    document.getElementById('addProduct').value = '';
+    all_product.innerHTML = "";
+    showProduct();
 
 }
-console.log(productArray);
-
