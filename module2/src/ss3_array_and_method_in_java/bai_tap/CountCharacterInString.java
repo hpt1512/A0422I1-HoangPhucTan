3a987fcfd88be2ca217f35a23e880d909dc59312
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-07-15 19:33:36 +0700

    Commit module2 ss3_array_and_method

---
+++ module2/src/ss3_array_and_method_in_java/bai_tap/CountCharacterInString.java
@@ -0,0 +1,24 @@
+package ss3_array_and_method_in_java.bai_tap;
+
+import java.util.Scanner;
+
+public class CountCharacterInString {
+    //    Đếm số lần xuất hiện của ký tự trong chuỗi
+    public static void main(String[] args) {
+        String str = "Hello World";
+        char character;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập ký tự: ");
+        character = scanner.nextLine().charAt(0);
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (character == str.charAt(i)) {
+                count++;
+            }
+        }
+
+        System.out.println("Ký tự " + character + " xuất hiện " + count + " lần trong chuỗi " + str);
+    }
+}
---
+++ module2/src/ss3_array_and_method_in_java/bai_tap/CountSumColumn.java
@@ -0,0 +1,33 @@
+package ss3_array_and_method_in_java.bai_tap;
+
+import java.util.Scanner;
+
+public class CountSumColumn {
+    //    Tính tổng các số ở một cột xác định
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        int sum = 0;
+
+//        Nhập cột muốn tính
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập cột muốn tính: ");
+        int index_col = scanner.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (j == index_col) {
+                    sum = sum + arr[i][index_col];
+                    System.out.print(arr[i][index_col] + "\t");
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Tổng các số trong cột " + index_col + " là: " + sum);
+    }
+}
