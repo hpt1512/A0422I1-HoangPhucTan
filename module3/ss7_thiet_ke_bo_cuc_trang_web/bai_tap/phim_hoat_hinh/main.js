0c1bc955f6865fccb4f5f31287bbf21e21419785
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-10-12 19:50:25 +0700

    Commit module 3 ss7

---
+++ module3/ss7_thiet_ke_bo_cuc_trang_web/bai_tap/phim_hoat_hinh/main.js
@@ -0,0 +1,20 @@
+let cartoon__item__1 = document.getElementById("cartoon__item__1");
+let cartoon__item__2 = document.getElementById("cartoon__item__2");
+let cartoon__item__3 = document.getElementById("cartoon__item__3");
+let cartoon__item__4 = document.getElementById("cartoon__item__4");
+
+let cartoon__div__1 = document.getElementsByClassName("cartoon__div__1");
+let cartoon__div__2 = document.getElementsByClassName("cartoon__div__2");
+let cartoon__div__3 = document.getElementsByClassName("cartoon__div__3");
+let cartoon__div__4 = document.getElementsByClassName("cartoon__div__4");
+
+
+
+cartoon__item__1.addEventListener('click', function() {
+    cartoon__div__1.classList.add('display--block');
+});
+
+
+
+
+
