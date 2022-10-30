c9bbbf42704280263e039cd9f9c078c061ce32c7
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2022-10-30 11:19:38 +0700

    Commit module3 ss12_jdbc

---
+++ module3/ss12_jdbc/user-management/src/main/java/com/example/usermanagement/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.usermanagement.service;
+
+import com.example.usermanagement.model.User;
+import com.example.usermanagement.repository.IUserRepository;
+import com.example.usermanagement.repository.UserRepositoryImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImpl implements IUserService {
+    private IUserRepository userRepository = new UserRepositoryImpl();
+    @Override
+    public void insertUser(User user) throws SQLException {
+        userRepository.insertUser(user);
+    }
+
+    @Override
+    public User selectUser(int id) {
+        return userRepository.selectUser(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return userRepository.selectAllUsers();
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        return userRepository.deleteUser(id);
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        return userRepository.updateUser(user);
+    }
+}
