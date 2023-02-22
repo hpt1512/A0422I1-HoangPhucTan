9aa301ee81d52161af73eea54e7fe5127ba54bbb
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2023-02-22 21:26:23 +0700

    commit ss5 task 1 casestudy

--- module5/casestudy/angular-furama-management/src/app/app.module.ts
+++ module5/casestudy/angular-furama-management/src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ServiceComponent } from './service/service.component';
+import { ServiceComponent } from './service/service-list/service.component';
+import { EditServiceComponent } from './service-edit/edit-service.component';
+import { ServiceCreateComponent } from './service/service-create/service-create.component';
+import { ServiceEditComponent } from './service/service-edit/service-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ServiceComponent } from './service/service.component';
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
-    ServiceComponent
+    ServiceComponent,
+    EditServiceComponent,
+    ServiceCreateComponent,
+    ServiceEditComponent
   ],
   imports: [
     BrowserModule,
