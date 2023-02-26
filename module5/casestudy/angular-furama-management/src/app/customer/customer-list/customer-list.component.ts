165d02c2b42176b44c16dccf0507f5190da91ae8
Hoang Phuc Tan <hoangphuctanpt@gmail.com>
2023-02-26 21:13:20 +0700

    Commit m5 task 3 casestudy

--- module5/casestudy/angular-furama-management/src/app/customer/customer-list/customer-list.component.ts
+++ module5/casestudy/angular-furama-management/src/app/customer/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Customer} from '../../customer';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,7 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
-
+  customers: Customer[] = [
+    {
+      id: 1,
+      customerType: {id: 1, name: 'Diamond'},
+      name: 'Hoàng Phúc Tân',
+      birthday: '2000-12-15',
+      gender: true,
+      idCard: '044200001835',
+      phoneNumber: '0889152558',
+      email: 'hoangphuctanpt@gmail.com',
+      address: '60/1 Lê Thị Tính'
+    },
+    {
+      id: 1,
+      customerType: {id: 2, name: 'Gold'},
+      name: 'Hoàng Anh Trường',
+      birthday: '2000-09-22',
+      gender: true,
+      idCard: '086394713746',
+      phoneNumber: '0123456789',
+      email: 'anhtruong@gmail.com',
+      address: '100 Cù Chính Lan'
+    },
+    {
+      id: 1,
+      customerType: {id: 3, name: 'Silva'},
+      name: 'Nguyễn Phương Linh',
+      birthday: '2003-12-15',
+      gender: false,
+      idCard: '012345678909876',
+      phoneNumber: '0912345678',
+      email: 'phươnglinh@gmail.com',
+      address: '200 Nguyển Phươc Nguyên'
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- module5/casestudy/angular-furama-management/src/app/service/service-list/service-list.component.ts
+++ module5/casestudy/angular-furama-management/src/app/service/service-list/service-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Facility} from '../../facility';
 
 @Component({
   selector: 'app-service-list',
@@ -6,7 +7,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-list.component.css']
 })
 export class ServiceListComponent implements OnInit {
-
+  facilities: Facility[] = [
+    {
+      id: 1,
+      name: 'Villa A1 hướng biển',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'Villa'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+    {
+      id: 2,
+      name: 'Room 205',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'Room'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+    {
+      id: 3,
+      name: 'House VIP',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'House'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+    {
+      id: 4,
+      name: 'Studio hướng biển',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'Villa'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+    {
+      id: 5,
+      name: 'Studio hướng biển',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'Villa'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+    {
+      id: 6,
+      name: 'Studio hướng biển',
+      image: '../assets/img/service/service1.png',
+      area: 100,
+      cost: 100,
+      peopleMax: 5,
+      rentType: {id: 1, name: 'Month', cost: 200},
+      serviceType: {id: 1, name: 'Villa'},
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'Bếp nướng',
+      poolArea: 20,
+      floor: 2
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
